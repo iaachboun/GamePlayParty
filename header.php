39896a1cc85a110ae01fe7e88925ebd1e8a56612
Chen pei Hu <chenpeihu6@gmail.com>
2019-09-13 23:01:03 +0200

    homepage mooier

--- header.php
+++ header.php
@@ -23,12 +23,12 @@
 </head>
 <body>
 <div class="logo-container">
-    <img class="logo" src="assets/img/logo.svg" alt="logo">
+    <a href="?pagina=home"><img class="logo" src="assets/img/logo.svg" alt="logo"></a>
 </div>
-<nav class="navbar navbar-expand-lg navbar-light justify-content-end">
+<nav class="navbar navbar-expand-lg navbar-dark justify-content-end">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon text-white"><i class="fa fa-navicon"></i></span>
+        <span class="navbar-dark navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul class="navbar-nav">
