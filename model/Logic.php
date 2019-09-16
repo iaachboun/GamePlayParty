92c32fa4f491499fe8c7ef0ebb945803bc6aa6f1
ilias <ili.aachboun1@gmail.com>
2019-09-16 20:28:16 +0200

    fix  responsiveness

--- model/Logic.php
+++ model/Logic.php
@@ -6,7 +6,7 @@ class Logic
     public function __construct()
     {
 
-        $this->DataHandler = new DataHandler("localhost", "mysql", "gpp", "root", "");
+        $this->DataHandler = new DataHandler("localhost", "mysql", "GamePlayParty", "ilias", "12345");
 
     }
 
